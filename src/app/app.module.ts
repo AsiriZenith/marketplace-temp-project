2b04f3f998cbe37c43bba98516682958a0ea7d1d
AsiriZenith <asirisenith@gmail.com>
2021-10-26 12:44:12 +0530

    added full proj first time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatStepperModule } from '@angular/material/stepper';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { ExampleComponent } from './component/example/example.component';
+import { NewApplicationComponent } from './component/new-application/new-application.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExampleComponent,
+    NewApplicationComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    MatIconModule,
+    MatCardModule,
+    MatTabsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatStepperModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
