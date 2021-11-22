3ccf6580673b83ac84fb75655df4186c68a8f4fc
AsiriZenith <asirisenith@gmail.com>
2021-11-22 15:53:59 +0530

    image upload part 2

--- src/app/component/image-upload-part2/image-upload2.component.ts
+++ src/app/component/image-upload-part2/image-upload2.component.ts
@@ -1,13 +1,34 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { FileUploadService } from '../../service/file-upload.service';
+
+class ImageSnippet {
+  constructor(public src: string, public file: File) {}
+}
 
 @Component({
   selector: 'image-upload-part2',
-  templateUrl:'./image-upload2.component.html',
-  styleUrls:['./image-upload2.component.css']
+  templateUrl: './image-upload2.component.html',
+  styleUrls: ['./image-upload2.component.css'],
 })
 export class ImageUploadPart2Component implements OnInit {
+  selectedFile!: ImageSnippet;
 
-  ngOnInit(): void {
+  constructor(private imageService: FileUploadService) {}
+
+  ngOnInit(): void {}
+
+  processFile(imageInput: any) {
+    const file: File = imageInput.files[0];
+    const reader = new FileReader();
+
+    reader.addEventListener('load', (event: any) => {
+      this.selectedFile = new ImageSnippet(event.target.result, file);
+
+      this.imageService.uploadImage(this.selectedFile.file).subscribe(
+        (res) => {},
+        (err) => {}
+      );
+    });
+    reader.readAsDataURL(file);
   }
-
 }
--- src/app/service/file-upload.service.ts
+++ src/app/service/file-upload.service.ts
@@ -1,24 +1,38 @@
-import { HttpClient, HttpEvent, HttpRequest } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import {
+  HttpClient,
+  HttpEvent,
+  HttpHeaders,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'multipart/form-data',
+    Authorization: 'Bearer 46ad3b06-c180-337c-a858-d1e90cb0d077',
+  }),
+};
 
 @Injectable({
-  providedIn:'root'
+  providedIn: 'root',
 })
 export class FileUploadService {
+  image_upload_url: string;
+  private baseUrl = 'http://localhost:9991';
 
-  private baseUrl = 'http://localhost:8080';
-
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.image_upload_url = `http://localhost:9991/application/uploadImageFile`;
+  }
 
   upload(file: File): Observable<HttpEvent<any>> {
     const formData: FormData = new FormData();
 
-    formData.append('file', file);
+    formData.append('file', file, file.name);
 
     const req = new HttpRequest('POST', `${this.baseUrl}/upload`, formData, {
       reportProgress: true,
-      responseType: 'json'
+      responseType: 'json',
     });
 
     return this.http.request(req);
@@ -26,5 +40,13 @@ export class FileUploadService {
 
   getFiles(): Observable<any> {
     return this.http.get(`${this.baseUrl}/files`);
+  }
+
+  public uploadImage(image: File): Observable<any> {
+    const formData = new FormData();
+
+    formData.append('image', image);
+
+    return this.http.post<any>(this.image_upload_url, formData, httpOptions);
   }
 }
