f94567ef499cb2670a3eddbeb0e23636b936350e
AsiriZenith <asirisenith@gmail.com>
2022-02-14 11:47:45 +0530

    video upload code changes

--- src/app/component/video-upload/video.upload.component.ts
+++ src/app/component/video-upload/video.upload.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+class VideoSnippet {
+  constructor(public src: string, public file: File) {}
+}
 
 @Component({
-  selector:'app-video-upload',
-  templateUrl:'./video.upload.component.html',
-  styleUrls:['./video.upload.component.css']
+  selector: 'app-video-upload',
+  templateUrl: './video.upload.component.html',
+  styleUrls: ['./video.upload.component.css'],
 })
-
 export class VideoUploadComponent implements OnInit {
+  public url: any;
+  videoToUpload!: VideoSnippet;
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSelectFile(event: any) {
+    const file = event.target.files && event.target.files[0];
+    if (file) {
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+
+      reader.onload = (event) => {
+        this.url = (<FileReader>event.target).result;
+
+      };
+    }
   }
-
 }
